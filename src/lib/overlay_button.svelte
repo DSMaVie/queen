e71bbedb83075e76d6bda71abf8b17a397f0a284
Max Vieweg <max.vieweg@outlook.com>
2023-04-08 23:10:08 +0200

    made overlay button classable

--- src/lib/overlay_button.svelte
+++ src/lib/overlay_button.svelte
@@ -9,10 +9,7 @@
 	};
 </script>
 
-<button
-	class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 rounded-full font-display font-bold flex-basis-1/4"
-	on:click={change_overlay_state}>{text}</button
->
+<button class={$$props.class} on:click={change_overlay_state}>{text}</button>
 {#if isOverlayOpen}
 	<div
 		class="w-screen h-screen fixed top-0 left-0 flex justify-center items-center bg-gray-900 opacity-50 z-10"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,9 @@
 </script>
 
 <div class="flex flex-row justify-center">
-	<OverlayButton text="Add Event"
+	<OverlayButton
+		text="Add Event"
+		class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 rounded-full font-display font-bold flex-basis-1/4"
 		><div class="bg-white max-h-400 p-4 rounded-md">Hooray</div></OverlayButton
 	>
 </div>
