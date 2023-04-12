5951aa7e66637a7a14344ccb30f4d185ce79a6b3
Max Vieweg <max.vieweg@outlook.com>
2023-04-12 13:31:27 +0200

    installed flowbite

---
+++ src/lib/components/new_event_form.svelte
@@ -0,0 +1,5 @@
+<form action="/add_event" method="post">
+<h1> Add Event</h1>
+<
+
+</form>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [
+    './src/**/*.{html,js,svelte,ts}',
+    "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -8,5 +11,8 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
+  darkMode: 'class',
 }
